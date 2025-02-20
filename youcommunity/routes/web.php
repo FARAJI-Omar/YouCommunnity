372f0016aa0101c8d66cceafb62dc86ba20a93bb
F99 <farajiomar99@gmail.com>
2025-02-20 11:06:23 +0100

    add routes for profile management, event handling, RSVP, and comments

---
+++ youcommunity/routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\RSVPController;
+use App\Http\Controllers\CommentController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Custom route for deleting events via "My Events"
+    Route::delete('events/my/{event}/destroy', [EventController::class, 'destroyMy'])->name('events.my.destroy');
+
+    Route::get('events/my', [EventController::class, 'myEvents'])->name('events.my');
+
+    Route::resource('events', EventController::class);
+
+    Route::post('events/{event}/rsvp', [RSVPController::class, 'store'])->name('rsvp.store');
+    Route::delete('events/{event}/rsvp', [RSVPController::class, 'destroy'])->name('rsvp.destroy');
+
+    Route::post('events/{event}/comments', [CommentController::class, 'store'])->name('comments.store');
+    Route::delete('comments/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy');
+});
+
+require __DIR__ . '/auth.php';
