27cfe50499cb6c1023d81620565535a001e28613
F99 <farajiomar99@gmail.com>
2025-03-17 12:16:49 +0000

    Update routes to improve event management and welcome page access

--- youcommunity/routes/web.php
+++ youcommunity/routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\RSVPController;
 use App\Http\Controllers\CommentController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [EventController::class, 'homePageEvents']);
+Route::get('/', [EventController::class, 'welcome']);
 
 Route::get('/dashboard', [EventController::class, 'homePageEvents'])->middleware(['auth', 'verified'])->name('dashboard');
 
@@ -15,20 +15,21 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    // Custom route for deleting events via "My Events"
     Route::delete('events/my/{event}/destroy', [EventController::class, 'destroyMy'])->name('events.my.destroy');
+    Route::get('events/my/{event}/edit', [EventController::class, 'edit'])->name('events.edit');
+    Route::patch('events/my/{event}', [EventController::class, 'update'])->name('events.update');
 
     Route::get('events/my', [EventController::class, 'myEvents'])->name('events.my');
 
     Route::resource('events', EventController::class);
+
+    Route::get('events/{event}', [EventController::class, 'show'])->name('events.details');
 
     Route::post('events/{event}/rsvp', [RSVPController::class, 'store'])->name('rsvp.store');
     Route::delete('events/{event}/rsvp', [RSVPController::class, 'destroy'])->name('rsvp.destroy');
 
     Route::post('events/{event}/comments', [CommentController::class, 'store'])->name('comments.store');
     Route::delete('comments/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy');
-
-    Route::get('events/{event}/details', [EventController::class, 'eventDetails'])->name('events.details');
 });
 
 require __DIR__ . '/auth.php';
